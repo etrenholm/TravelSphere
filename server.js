6af6a89f36f2268884a3b74ca41aec536fc58348
Erica <erica.trenholm48@gmail.com>
2022-05-05 22:44:41 -0400

    added helpers for formatting plural

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
 const path = require('path');
 
 // ansi-colors
@@ -22,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // start handlebars
-const hbs = exphbs.create({ });
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
